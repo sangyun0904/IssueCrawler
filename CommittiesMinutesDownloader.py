7f479e3adc30a4aa8d6ba1f67e76e9aea8386087
sangyoon kim <sangyun0904@gmail.com>
2025-06-04 10:58:34 +0900

    국회 회의록 다운로드 및 파일 저장 코드 추가

--- CommittiesMinutesDownloader.py
+++ CommittiesMinutesDownloader.py
@@ -16,17 +16,34 @@ def download_committee_minutes(pIndex):
         print(f"Error: {response.status_code}")
         return None
     
-# def save_committee_minutes(data, filename):
-#     if data and 'result' in data and 'items' in data['result']:
-#         items = data['VCONFSPCCONFLIST'][2]["row"]
-#         with open(filename, 'w', encoding='utf-8') as file:
-#             for item in items:
-#                 file.write(f"{item}\n")
-#     else:
-#         print("No data to save or data format is incorrect.")
+def save_committee_minutes(data):
+    urls = []
+    if data and 'VCONFSPCCONFLIST' in data and 'row' in data['VCONFSPCCONFLIST'][1]:
+        items = data['VCONFSPCCONFLIST'][1]['row']
+        for item in items:
+            title = "_".join([item['ERACO'], item['CMIT_NM'], item['DGR'], item['CONF_DT']]) + ".pdf"
+            file_url = item['DOWN_URL']
+            urls.append(file_url)
+            save_to_resources(file_url, title)
+    else:
+        print("No data to save or data format is incorrect.")
+    return urls
+
+def save_to_resources(url, file_name):
+    resources_dir = "resources"
+    os.makedirs(resources_dir, exist_ok=True)  # 폴더가 없으면 생성
+    file_path = os.path.join(resources_dir, file_name)
+
+    response = requests.get(url, stream=True)
+    if response.status_code == 200:
+        with open(file_path, 'wb') as file:
+            for chunk in response.iter_content(chunk_size=8192):
+                file.write(chunk)
+        print(f"File saved: {file_path}")
+    else:
+        print(f"Failed to download file from {url}. Status code: {response.status_code}")
 
 if __name__ == "__main__":
     pIndex = 1  # 페이지 인덱스 설정
     committee_minutes = download_committee_minutes(pIndex)
-    
-    print(committee_minutes)
+    save_committee_minutes(committee_minutes)
